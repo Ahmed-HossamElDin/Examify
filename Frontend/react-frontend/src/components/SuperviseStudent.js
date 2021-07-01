bda75537124d6b7bc7d1c0d367f034da4bdb3f68
Ahmad Ramzy <ahmadramzy988@gmail.com>
2021-07-01 17:06:55 +0200

    Added placeholder video

--- Frontend/react-frontend/src/components/SuperviseStudent.js
+++ Frontend/react-frontend/src/components/SuperviseStudent.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItem from "@material-ui/core/ListItem";
 import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -27,7 +23,7 @@ export default class SuperviseStudent extends Component {
       this.setState({ open: false });
     };
     return (
-      <div>
+      <div style={{ height: "25%", width: "25%" }}>
         <Card
           style={{
             maxWidth: 345,
@@ -36,16 +32,31 @@ export default class SuperviseStudent extends Component {
           onClick={handleClickOpen}
         >
           <CardContent>
-            {" "}
-            <CardMedia
-              style={{ cursor: " zoom-in" }}
-              component="img"
-              height={"25%"}
-              width={"15%"}
-              alt={this.props.info.student_name}
-              image="https://i.postimg.cc/RVHCzC1N/image.png"
-              title={this.props.info.student_name}
-            />{" "}
+            <div
+              style={{
+                width: "100%",
+                height: "0px",
+                position: "relative",
+                paddingBottom: "56.250%",
+              }}
+            >
+              <iframe
+                src="https://streamable.com/e/f0qajc?autoplay=1&nocontrols=1"
+                frameborder="0"
+                width="100%"
+                height="100%"
+                allowfullscreen
+                allow="autoplay"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  left: "0px",
+                  top: "0px",
+                  overflow: "hidden",
+                }}
+              ></iframe>
+            </div>
             <CardContent>
               <Typography gutterBottom variant="h5" component="h2">
                 {this.props.info.student_name}'s Camera
@@ -74,14 +85,38 @@ export default class SuperviseStudent extends Component {
               </Typography>
             </Toolbar>
           </AppBar>
-          <List>
-            <CardMedia
+          {/*<CardMedia
               component="img"
               alt={this.props.student_name}
               image="https://i.postimg.cc/RVHCzC1N/image.png"
               title={this.props.student_name}
-            />
-          </List>
+            />{" "}*/}
+
+          <div
+            style={{
+              width: "100%",
+              height: "0px",
+              position: "relative",
+              paddingBottom: "56.250%",
+            }}
+          >
+            <iframe
+              src="https://www.youtube.com/embed/jbUI2d6RwE8?autoplay=1&nocontrols=1"
+              frameborder="0"
+              width="100%"
+              height="100%"
+              allowfullscreen
+              allow="autoplay"
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                left: "0px",
+                top: "0px",
+                overflow: "hidden",
+              }}
+            ></iframe>
+          </div>
         </Dialog>
       </div>
     );
