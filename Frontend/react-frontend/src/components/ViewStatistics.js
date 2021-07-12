342a3af840b27a06561dffefd7c884493961404c
sefdf01 <34137064+sefdf01@users.noreply.github.com>
2021-07-12 14:28:03 +0200

    Update ViewStatistics.js

--- Frontend/react-frontend/src/components/ViewStatistics.js
+++ Frontend/react-frontend/src/components/ViewStatistics.js
@@ -172,27 +172,27 @@ class ViewStatistics extends Component {
               Total Mark: {this.state.exam_stats.total_mark} <br />
               Average:{" "}
               {this.state.exam_statistics.avg !== null &&
-              this.state.exam_statistics.avg !== undefined
+                this.state.exam_statistics.avg !== undefined
                 ? " " + this.state.exam_statistics.avg + " "
                 : " 0"}{" "}
               Max Mark:
               {this.state.exam_statistics.max !== null &&
-              this.state.exam_statistics.max !== undefined
+                this.state.exam_statistics.max !== undefined
                 ? " " + this.state.exam_statistics.max + " "
                 : " 0"}{" "}
               Min Mark:
               {this.state.exam_statistics.min !== null &&
-              this.state.exam_statistics.min !== undefined
+                this.state.exam_statistics.min !== undefined
                 ? " " + this.state.exam_statistics.min + " "
                 : " 0"}{" "}
               Number of Submits:{" "}
               {this.state.exam_statistics.num_of_students_submited_the_exam !==
                 null &&
-              this.state.exam_statistics.num_of_students_submited_the_exam !==
+                this.state.exam_statistics.num_of_students_submited_the_exam !==
                 undefined
                 ? " " +
-                  this.state.exam_statistics.num_of_students_submited_the_exam +
-                  " "
+                this.state.exam_statistics.num_of_students_submited_the_exam +
+                " "
                 : " 0"}
             </p>
           </Jumbotron>
@@ -263,188 +263,187 @@ class ViewStatistics extends Component {
               margin: "20px",
             }}
           >
-            
+
           </div>
         </div>
         <div>
+        <h1>Individual questions statistics</h1>
           {Object.keys(this.state.question_stats).map((key) => {
             return (
-              <div>
-                <h1>Individual questions statistics</h1>
-                <div
+            <div
+              style={{
+                float: "left",
+                direction: "flex",
+                marginRight: 2,
+                marginTop: 5,
+                borderStyle: "solid",
+                borderRadius: 15,
+              }}
+              key={key}
+            >
+              <div
                 style={{
-                  float: "left",
-                  direction: "flex",
-                  marginRight: 2,
-                  marginTop: 5,
-                  borderStyle: "solid",
-                  borderRadius: 15,
+                  marginLeft: 20,
+                  marginTop: 20,
+                  marginBottom: 20,
+                  marginRight: 20,
                 }}
-                key={key}
               >
-                <div
-                  style={{
-                    marginLeft: 20,
-                    marginTop: 20,
-                    marginBottom: 20,
-                    marginRight: 20,
-                  }}
-                >
-                  <Card bg={"Light"}>
-                    <Card.Header>
-                      {parseInt(key) +
-                        1 +
-                        ". " +
-                        this.state.question_stats[key].text +
+                <Card bg={"Light"}>
+                  <Card.Header>
+                    {parseInt(key) +
+                      1 +
+                      ". " +
+                      this.state.question_stats[key].text +
+                      " "}
+                    <hr />
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Title>
+                      {"Correct Answer: " +
+                        this.state.question_stats[key].correct_answer +
                         " "}
-                      <hr />
-                    </Card.Header>
-                    <Card.Body>
-                      <Card.Title>
-                        {"Correct Answer: " +
-                          this.state.question_stats[key].correct_answer +
-                          " "}
-                      </Card.Title>
-                      <Card.Text>
-                        {"Correct Answers count: " +
-                          this.state.question_stats[key].correct_count +
-                          " student(s)"}
-                        <br />
-                        {"Incorrect Answers count: " +
+                    </Card.Title>
+                    <Card.Text>
+                      {"Correct Answers count: " +
+                        this.state.question_stats[key].correct_count +
+                        " student(s)"}
+                      <br />
+                      {"Incorrect Answers count: " +
+                        this.state.question_stats[key].wrong_count +
+                        " student(s)"}
+                      <br />
+                      {"Unanswered count: " +
+                        parseInt(
+                          this.state.exam_statistics
+                            .num_of_students_submited_the_exam -
                           this.state.question_stats[key].wrong_count +
-                          " student(s)"}
-                        <br />
-                        {"Unanswered count: " +
-                          parseInt(
+                          this.state.question_stats[key].correct_count
+                        ) +
+                        " student(s)"}{" "}
+                    </Card.Text>
+                    <ProgressBar>
+                      <ProgressBar
+                        striped
+                        style={{ backgroundColor: (0, 128, 255) }}
+                        now={
+                          (this.state.question_stats[key].correct_count /
                             this.state.exam_statistics
-                              .num_of_students_submited_the_exam -
-                              this.state.question_stats[key].wrong_count +
-                              this.state.question_stats[key].correct_count
-                          ) +
-                          " student(s)"}{" "}
-                      </Card.Text>
-                      <ProgressBar>
+                              .num_of_students_submited_the_exam) *
+                          100
+                        }
+                        label={
+                          (this.state.question_stats[key].correct_count /
+                            this.state.exam_statistics
+                              .num_of_students_submited_the_exam) *
+                          100 +
+                          "%"
+                        }
+                      />
+                      <ProgressBar
+                        striped
+                        style={{
+                          backgroundColor: "#CC0000",
+                        }}
+                        now={
+                          (this.state.question_stats[key].wrong_count /
+                            this.state.exam_statistics
+                              .num_of_students_submited_the_exam) *
+                          100
+                        }
+                        label={
+                          (this.state.question_stats[key].wrong_count /
+                            this.state.exam_statistics
+                              .num_of_students_submited_the_exam) *
+                          100 +
+                          "%"
+                        }
+                      />{" "}
+                      <ProgressBar
+                        striped
+                        style={{
+                          backgroundColor: "#CCCC00",
+                        }}
+                        now={
+                          ((this.state.exam_statistics
+                            .num_of_students_submited_the_exam -
+                            this.state.question_stats[key].wrong_count +
+                            this.state.question_stats[key].correct_count) /
+                            this.state.exam_statistics
+                              .num_of_students_submited_the_exam) *
+                          100
+                        }
+                        label={
+                          (this.state.question_stats[key].wrong_count /
+                            this.state.exam_statistics
+                              .num_of_students_submited_the_exam) *
+                          100 +
+                          "%"
+                        }
+                      />{" "}
+                    </ProgressBar>{" "}
+                    <Card>
+                      {" "}
+                      <div
+                        style={{
+                          float: "right",
+                          marginRight: "22%",
+                        }}
+                      >
+                        Correct answer
+                      </div>
+                      <ProgressBar style={{ width: 50 }}>
                         <ProgressBar
                           striped
-                          style={{ backgroundColor: (0, 128, 255) }}
-                          now={
-                            (this.state.question_stats[key].correct_count /
-                              this.state.exam_statistics
-                                .num_of_students_submited_the_exam) *
-                            100
-                          }
-                          label={
-                            (this.state.question_stats[key].correct_count /
-                              this.state.exam_statistics
-                                .num_of_students_submited_the_exam) *
-                              100 +
-                            "%"
-                          }
+                          style={{
+                            backgroundColor: (0, 128, 255),
+                          }}
+                          now={100}
                         />
+                      </ProgressBar>{" "}
+                      <div
+                        style={{
+                          float: "right",
+                          marginRight: "18%",
+                          textAlign: "center",
+                        }}
+                      >
+                        Incorrect answer
+                      </div>
+                      <ProgressBar style={{ width: 50 }}>
                         <ProgressBar
                           striped
                           style={{
                             backgroundColor: "#CC0000",
                           }}
-                          now={
-                            (this.state.question_stats[key].wrong_count /
-                              this.state.exam_statistics
-                                .num_of_students_submited_the_exam) *
-                            100
-                          }
-                          label={
-                            (this.state.question_stats[key].wrong_count /
-                              this.state.exam_statistics
-                                .num_of_students_submited_the_exam) *
-                              100 +
-                            "%"
-                          }
+                          now={100}
                         />{" "}
+                      </ProgressBar>{" "}
+                      <div
+                        style={{
+                          float: "right",
+                          marginRight: "30%",
+                          textAlign: "center",
+                        }}
+                      >
+                        Unanswered
+                      </div>
+                      <ProgressBar style={{ width: 50 }}>
                         <ProgressBar
                           striped
                           style={{
                             backgroundColor: "#CCCC00",
                           }}
-                          now={
-                            ((this.state.exam_statistics
-                              .num_of_students_submited_the_exam -
-                              this.state.question_stats[key].wrong_count +
-                              this.state.question_stats[key].correct_count) /
-                              this.state.exam_statistics
-                                .num_of_students_submited_the_exam) *
-                            100
-                          }
-                          label={
-                            (this.state.question_stats[key].wrong_count /
-                              this.state.exam_statistics
-                                .num_of_students_submited_the_exam) *
-                              100 +
-                            "%"
-                          }
+                          now={100}
                         />{" "}
                       </ProgressBar>{" "}
-                      <Card>
-                        {" "}
-                        <div
-                          style={{
-                            float: "right",
-                            marginRight: "22%",
-                          }}
-                        >
-                          Correct answer
-                        </div>
-                        <ProgressBar style={{ width: 50 }}>
-                          <ProgressBar
-                            striped
-                            style={{
-                              backgroundColor: (0, 128, 255),
-                            }}
-                            now={100}
-                          />
-                        </ProgressBar>{" "}
-                        <div
-                          style={{
-                            float: "right",
-                            marginRight: "18%",
-                            textAlign: "center",
-                          }}
-                        >
-                          Incorrect answer
-                        </div>
-                        <ProgressBar style={{ width: 50 }}>
-                          <ProgressBar
-                            striped
-                            style={{
-                              backgroundColor: "#CC0000",
-                            }}
-                            now={100}
-                          />{" "}
-                        </ProgressBar>{" "}
-                        <div
-                          style={{
-                            float: "right",
-                            marginRight: "30%",
-                            textAlign: "center",
-                          }}
-                        >
-                          Unanswered
-                        </div>
-                        <ProgressBar style={{ width: 50 }}>
-                          <ProgressBar
-                            striped
-                            style={{
-                              backgroundColor: "#CCCC00",
-                            }}
-                            now={100}
-                          />{" "}
-                        </ProgressBar>{" "}
-                      </Card>
-                    </Card.Body>
-                  </Card>
-                </div>
+                    </Card>
+                  </Card.Body>
+                </Card>
               </div>
-              </div>
-              
+            </div>
+
+
             );
           })}{" "}
         </div>
